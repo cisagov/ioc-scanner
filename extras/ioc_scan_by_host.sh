82bd25723dbde5e396fc0fbf4a1bd20edf93cf1c
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-06-06 12:41:54 -0400

    Improve the remote command
    * Use find-grep instead of relying on shell globbing.
    * Get rid of one of the grep filters by filtering the search in find.

--- extras/ioc_scan_by_host.sh
+++ extras/ioc_scan_by_host.sh
@@ -52,12 +52,13 @@ for instance_id in "${instances[@]}"; do
   echo
   echo Searching "$instance_id":
 
-  # Use grep to search for IOC strings in log files.  We use the --invert-match
-  # grep flag to exclude files that contain our grep command (e.g. sudo.log) and
-  # also to exclude .journal file matches.
+  # Use find-grep to search for IOC strings in log files, ignoring
+  # *.journal files.  We pipe the result into another grep process
+  # that uses the --invert-match grep flag to exclude matches (e.g.,
+  # from sudo.log) that contain our grep command (e.g. sudo.log).
   aws ssm start-session --target="$instance_id" \
     --document=AWS-StartInteractiveCommand \
-    --parameters="command='for i in ${iocList[*]}; do sudo grep --ignore-case --recursive --extended-regexp \$i /var/log/* | grep --invert-match ignore-case | grep  --invert-match .journal | echo \$(wc --lines) found for \$i; done'"
+    --parameters="command='for i in ${iocList[*]}; do sudo find /var/log -type f -not -name \*\.journal -exec grep --ignore-case --recursive --extended-regexp \$i {} \; | grep --invert-match -- --ignore-case\ --recursive\ --extended-regexp | echo \$(wc --lines) found for \$i; done'"
 
   echo Search of "$instance_id" is complete.
 done
