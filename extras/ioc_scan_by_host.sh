2bd2f6548b3aa8dd1f00384b3e886a92fe884e9c
bra1ncramp <mjgolla@outlook.com>
2023-11-01 10:47:51 -0400

    Improve log output
    Provide hostname of instance for more clarity in log output

--- extras/ioc_scan_by_host.sh
+++ extras/ioc_scan_by_host.sh
@@ -50,7 +50,7 @@ echo Instances are: "${instances[*]}"
 # Loop through all instance IDs
 for instance_id in "${instances[@]}"; do
   echo
-  echo Searching "$instance_id":
+  echo -n Searching "$instance_id":
 
   # Use find-grep to search for IOC strings in log files, ignoring
   # *.journal files.  We pipe the result into another grep process
@@ -58,7 +58,7 @@ for instance_id in "${instances[@]}"; do
   # from sudo.log) that contain our grep command (e.g. sudo.log).
   aws ssm start-session --target="$instance_id" \
     --document=AWS-StartInteractiveCommand \
-    --parameters="command='for i in ${iocList[*]}; do sudo find /var/log -type f -not -name \*\.journal -exec zgrep --ignore-case --extended-regexp \$i {} \; | grep --invert-match -- --ignore-case\ --recursive\ --extended-regexp\ | echo \$(wc --lines) found for \$i; done'"
+    --parameters="command='hostname; for i in ${iocList[*]}; do sudo find /var/log -type f -not -name \*\.journal -exec zgrep --ignore-case --extended-regexp \$i {} \; | grep --invert-match -- --ignore-case\ --recursive\ --extended-regexp\ | echo \$(wc --lines) found for \$i; done'"
 
   echo Search of "$instance_id" is complete.
 done
